578ed936a5ffb20fb30cd4031aaf0a2c52646324
Sean Kenny <sean.kenny@hotmail.com>
2014-07-31 23:12:42 +0100

    Pass the first 2 characters even if it's a space

--- sandbox/filecount/filecount.sh
+++ sandbox/filecount/filecount.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-_parse_change() {
-    changes=$1
-    file=$2
+GIT_STATUS_STAGED=0
+GIT_STATUS_ADDED=0
+GIT_STATUS_MODIFIED=0
+GIT_STATUS_REMOVED=0
+GIT_STATUS_UNTRACKED=0
 
-    echo ${changes:0:1}
+_parse_change() {
+    changes=$@
+
+    change1=${changes:0:1}
+    change2=${changes:1:2}
+    echo -$change1-
+    echo -$change2-
 }
 
 IFS=$'\n'
 status_arr=( $(git status --porcelain -b) )
-unset IFS
 
 unset status_arr[0]
 
-change_count=${#status_arr[@]}
+change_count=
 
-for (( i=1; i<=$change_count; i++ )); do
-    _parse_change ${status_arr[$i]}
+for (( i=1; i<=${#status_arr[@]}; i++ )); do
+    _parse_change ${status_arr[$i]:0:2}
 done
+
+unset IFS
