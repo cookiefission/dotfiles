8fb5ea130c74a89e1d2b289dca36d95e4bef0144
Sean Kenny <sean.kenny@hotmail.com>
2014-08-05 20:38:29 +0100

    Added a comment for posterity

--- sandbox/statusline/issue.sh
+++ sandbox/statusline/issue.sh
@@ -5,5 +5,7 @@ touch ab
 status_arr=( $(git status --porcelain -b) )
 for (( i=0; i<${#status_arr[@]}; i++ )); do
     echo ${status_arr[$i]}
+    # Without quotes, untracked ?? are interpreted by bash as the filename
+    # glob operator and the filename ab is expanded to
     echo "${status_arr[$i]:0:2}"
 done
