82b195d5569cc7c2275735608139d900b15833ac
Sean Kenny <sean.kenny@hotmail.com>
2014-08-05 16:54:53 +0100

    Proof(?) of bash bug

---
+++ sandbox/bashbug/proof.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IFS=$'\n'
+touch df
+status_arr=( $(git status --porcelain -b) )
+for (( i=0; i<${#status_arr[@]}; i++ )); do
+    echo ${status_arr[$i]}
+    echo ${status_arr[$i]:0:2}
+done
