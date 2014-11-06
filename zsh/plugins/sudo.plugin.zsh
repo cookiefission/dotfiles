db401600f329f5b1d725d32bd549b10d83f20525
Sean Kenny <sean.kenny@hotmail.com>
2014-11-06 13:08:27 +0000

    Reformat plugin file

--- zsh/plugins/sudo.plugin.zsh
+++ zsh/plugins/sudo.plugin.zsh
@@ -1,8 +1,10 @@
 sudo-command-line() {
-[[ -z $BUFFER ]] && zle up-history
-[[ $BUFFER != sudo\ * ]] && BUFFER="sudo $BUFFER"
-zle end-of-line
+    [[ -z $BUFFER ]] && zle up-history
+    [[ $BUFFER != sudo\ * ]] && BUFFER="sudo $BUFFER"
+    zle end-of-line
 }
+
 zle -N sudo-command-line
+
 # Defined shortcut keys: [Esc] [Esc]
 bindkey "\e\e" sudo-command-line
