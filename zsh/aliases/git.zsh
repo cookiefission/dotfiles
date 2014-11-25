46a71f428f4aa2afc0674ae7c8ea1d38d487c055
Sean Kenny <sean.kenny@hotmail.com>
2014-11-25 18:34:29 +0000

    Verbosity == Bad

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -1,11 +1,9 @@
 #!/bin/zsh
 
 alias -g groot='"$(git rev-parse --show-toplevel)"'
-alias changes='git status --porcelain | cut -c 4- | grep -v lock'
-alias unstagedchanges='git diff --name-only | grep -v lock'
-alias lastchanges='git diff-tree --no-commit-id --name-only -r HEAD | grep -v lock'
-alias openchanges='(cd groot && vim $(changes))'
-alias openunstagedchanges='(cd groot && vim $(unstagedchanges))'
-alias openlast='(cd groot && vim $(lastchanges))'
 alias repo='basename groot'
+alias chg='git status --porcelain | cut -c 4- | grep -v lock'
+alias lastchange='git diff-tree --no-commit-id --name-only -r HEAD | grep -v lock'
+alias ochg='(cd groot && vim $(chg))'
+alias olast='(cd groot && vim $(lastchange))'
 alias last-tag="git tag --sort=v:refname | egrep '^[v]*\d+(?:\.\d+)*' | tail -n 1"
