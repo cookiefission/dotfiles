b4fbd19a988057046ec41861f10f01c33ebe2ec6
Sean Kenny <sean.kenny@hotmail.com>
2014-12-03 13:17:10 +0000

    Compact

--- zsh/plugins/crap.zsh
+++ zsh/plugins/crap.zsh
@@ -4,9 +4,7 @@ crap() {
     CRAPPER="$@"
     NOTIFIER="terminal-notifier -message '$CRAPPER crashed out like a tit'"
 
-    if ! [ -n "$CRAPPER" ]; then
-        echo "No command given" && return 1
-    fi
+    [ -n "$CRAPPER" ] || return 1
 
     eval "$CRAPPER"
     eval "$NOTIFIER"
