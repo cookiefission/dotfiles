e84e6e4bf44a48e7a4dc7236ef1898c133c4f371
Sean Kenny <sean.kenny@hotmail.com>
2014-12-03 13:36:05 +0000

    Local vars

--- zsh/plugins/crap.zsh
+++ zsh/plugins/crap.zsh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
 crap() {
-    CRAPPER="$@"
-    NOTIFIER="terminal-notifier -message '$CRAPPER crashed out like a tit'"
+    local CRAPPER="$@"
+    local NOTIFIER="terminal-notifier -message '$CRAPPER crashed out like a tit'"
+
 
     [ -n "$CRAPPER" ] || return 1
 
