07b95668dfa6ebadca73b343cd2790e9b6344644
Sean Kenny <sean.kenny@hotmail.com>
2014-12-03 13:12:14 +0000

    Convert crap to function

---
+++ zsh/plugins/crap.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+crap() {
+    CRAPPER="$@"
+    NOTIFIER="terminal-notifier -message '$CRAPPER crashed out like a tit'"
+
+    if ! [ -n "$CRAPPER" ]; then
+        echo "No command given" && exit 1
+    fi
+
+    eval "$CRAPPER"
+    eval "$NOTIFIER"
+}
