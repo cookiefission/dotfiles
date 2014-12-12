e2cefbdea0f4cf53f237953d157ce0c68ef2f40f
Sean Kenny <sean.kenny@hotmail.com>
2014-12-12 10:58:03 +0000

    It works for now

---
+++ zsh/plugins/s.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+function s() {
+    if [ -z $TMUX ]; then
+        echo "Not running in tmux"
+        return 1
+    fi
+    local SPLIT_COMMAND="$*"
+    tmux split-window -v "$SPLIT_COMMAND; sleep 2" && tmux send-keys '^L'
+}
