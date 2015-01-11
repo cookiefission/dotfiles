be8771a0eb46f7fb657d00664a81ade414121b40
Sean Kenny <sean.kenny@hotmail.com>
2015-01-11 22:36:00 +0000

    Add view and layout helper

--- zsh/plugins/rails_helpers.zsh
+++ zsh/plugins/rails_helpers.zsh
@@ -8,3 +8,20 @@ rmodel() {
     fi
     vim -O app/models/$model.rb spec/models/${model}_spec.rb
 }
+
+rview() {
+    local view="$1"
+    if [[ -z "$view" ]]; then
+        echo "No view name provided"
+        return 1
+    fi
+    vim app/views/$view.html.erb
+}
+
+rlayout() {
+    local layout="$1"
+    if [[ -z "$layout" ]]; then
+        layout="application"
+    fi
+    vim app/views/layouts/$layout.html.erb
+}
