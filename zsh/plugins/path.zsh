d91e9cb8d982f26bad52795efa627e8f92f5c859
Sean Kenny <sean.kenny@hotmail.com>
2015-01-12 13:30:29 +0000

    Add composer global bin to PATH - plasma welder TV

--- zsh/plugins/path.zsh
+++ zsh/plugins/path.zsh
@@ -1,7 +1,15 @@
 #!/bin/zsh
 
+# Go loading
 export GOPATH=/Users/sean/go
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$GOPATH/bin
+
+# Add composer global bin to path
+export PATH=$HOME/.composer/vendor/bin:$PATH
+
+# Homebrew
 export PATH=/usr/local/bin:$PATH
+
+# MacPorts
 export PATH=/opt/local/bin:$PATH
