1b0645d0682250174b93e618f522b308cb6ed25d
Sean Kenny <sean.kenny@hotmail.com>
2015-02-24 14:46:09 +0000

    Add alias to open only conflicted files

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -6,5 +6,6 @@ alias chg='git status --porcelain | cut -c 4- | grep -v lock'
 alias lastchange='git diff-tree --no-commit-id --name-only -r HEAD | grep -v lock'
 alias ochg='(cd groot && vim $(chg))'
 alias olast='(cd groot && vim $(lastchange))'
+alias oconflicts='(cd groot && vim $(git diff --name-only --diff-filter=U))'
 alias last-tag="git tag --sort=v:refname | egrep '^[v]*\d+(?:\.\d+)*' | tail -n 1"
 alias glast='git show HEAD'
