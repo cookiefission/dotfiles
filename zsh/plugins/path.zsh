fd0a8975ec09435db9a1e519c02b8a7a769158f7
Sean Kenny <sean.kenny@hotmail.com>
2015-02-26 21:15:29 +0000

    Add ~/bin to PATH

--- zsh/plugins/path.zsh
+++ zsh/plugins/path.zsh
@@ -5,8 +5,11 @@ export GOPATH=/Users/sean/go
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$GOPATH/bin
 
-# Add composer global bin to path
+# Composer
 export PATH=$PATH:$HOME/.composer/vendor/bin
+
+# Home
+export PATH=$HOME/bin:$PATH
 
 # Homebrew
 export PATH=/usr/local/bin:$PATH
