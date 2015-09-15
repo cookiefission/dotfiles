c6b2781f0cd9114c0e12c4c9fd0dcd273957fa97
Sean Kenny <sean.kenny@hotmail.com>
2015-09-15 16:39:20 +0100

    Strip unused path out

--- zsh/plugins/path.zsh
+++ zsh/plugins/path.zsh
@@ -5,18 +5,12 @@ export GOPATH=/Users/sean/go
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$GOPATH/bin
 
-# Composer
-export PATH=$PATH:$HOME/.composer/vendor/bin
-
 # Home
 export PATH=$HOME/.bin:$PATH
 
 # Homebrew
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
-
-# MacPorts
-export PATH=/opt/local/bin:$PATH
 
 # RVM
 export PATH="$PATH:$HOME/.rvm/bin"
