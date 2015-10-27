b24bf9c6dfcb029bc65a8731102336435b945b6d
Sean Kenny <sean.kenny@hotmail.com>
2015-10-27 11:07:09 +0000

    Add note for future me so I don't change how vnote works

--- zsh/plugins/vnote.zsh
+++ zsh/plugins/vnote.zsh
@@ -1,5 +1,11 @@
 #!/bin/zsh
 
+# The duplication of `vim "$PWD/.notes.md"` is intentional.
+# If .notes.md exists in the current dir, I probably put it
+# there for a reason. Otherwise, create/open one at the root
+# of the git repo, if applicable. As a last resort, open
+# .notes.md in the current dir
+
 function vnote() {
     if [ -f "$PWD/.notes.md" ]; then
         vim "$PWD/.notes.md"
