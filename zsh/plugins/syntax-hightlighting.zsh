807d34c63c41141253063c85b3ae19f02eb67561
Sean Kenny <sean.kenny@hotmail.com>
2016-09-27 09:20:04 +0100

    Update zsh syntax highlighting to use homebrew
    And add it to Brewfile

--- zsh/plugins/syntax-hightlighting.zsh
+++ zsh/plugins/syntax-hightlighting.zsh
@@ -1,5 +1,5 @@
-if [ -d $HOME/.zsh-syntax-highlighting ]; then
-    source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [ -d /usr/local/share/zsh-syntax-highlighting ]; then
+    source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
     ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor line)
     ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
 fi
