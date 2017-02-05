496b9f5314c23aefd42fe0a6cfdeecb53404f9a6
Sean Kenny <sean.kenny@hotmail.com>
2017-02-05 19:07:31 +0000

    Remove some unused global aliases

--- zsh/aliases/global.zsh
+++ zsh/aliases/global.zsh
@@ -3,7 +3,5 @@
 alias -g JSON='| jq "."'
 alias -g XML='| xmllint --format -'
 alias -g G='| grep'
-alias -g P='>&2'
 alias -g LAST='| tail -n 1'
-alias -g ...='../../..'
 alias -g TRIM='| cut -c 1-$COLUMNS'
