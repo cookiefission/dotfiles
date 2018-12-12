0f8d9f41d6b1792f3c648299d7d37ba11991b9c7
Sean Kenny <sean.kenny@thebeansgroup.com>
2018-12-12 14:50:54 +0000

    Add zsh completions for Ubuntu
    The paths are different and kubectl isn't there for Ubuntu for some
    reason.

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -2,10 +2,16 @@
 
 if [ -d /usr/local/share/zsh-completions ]; then
     fpath=(/usr/local/share/zsh-completions $fpath)
+elif [ -d $HOME/.zsh-completions ]; then
+    fpath=($HOME/.zsh-completions $fpath)
 else
     echo "zsh-completions not installed. On OSX: brew install zsh-completions" >&2
 fi
 
 if [ -d ~/.zsh/completions ]; then
     fpath=(~/.zsh/completions $fpath)
+fi
+
+if [ $commands[kubectl] ]; then
+  source <(kubectl completion zsh)
 fi
