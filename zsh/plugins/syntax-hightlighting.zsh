f2d060055a2d29e4c9de5ff55d4b98ff3c659b52
Sean Kenny <sean.kenny@thebeansgroup.com>
2018-12-12 16:08:47 +0000

    Source syntax highlighting if clone to home
    This allows manual installation to be recognised. Previous version only
    supported installation via homebrew

--- zsh/plugins/syntax-hightlighting.zsh
+++ zsh/plugins/syntax-hightlighting.zsh
@@ -2,4 +2,8 @@ if [ -d /usr/local/share/zsh-syntax-highlighting ]; then
     source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
     ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor line)
     ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
+elif [ -d $HOME/.zsh-syntax-highlighting ]; then
+    source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor line)
+    ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
 fi
